3e5f419687f8984d2c99bf83c0a01a1fee4e1bf1
pro100ton <pro100ton@gmail.com>
2023-03-16 22:04:16 +0300

    Add AppLifeCycle project

---
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Антон Шалимов on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
