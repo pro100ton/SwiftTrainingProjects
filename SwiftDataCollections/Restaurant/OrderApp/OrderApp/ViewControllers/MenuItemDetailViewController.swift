9acd6cede47883ba0a69ad87a1cd1445d27d9f76
pro100ton <pro100ton@gmail.com>
2023-03-22 20:48:37 +0300

    OrderApp: Add animations to order button

--- SwiftDataCollections/Restaurant/OrderApp/OrderApp/ViewControllers/MenuItemDetailViewController.swift
+++ SwiftDataCollections/Restaurant/OrderApp/OrderApp/ViewControllers/MenuItemDetailViewController.swift
@@ -35,26 +35,42 @@ class MenuItemDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         updateUI()
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     // MARK: Helper methods
+    /// Метод для обновления UI VC
     func updateUI() {
         self.nameLabel.text = menuItem.name
         self.priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
         self.detailTextLabel.text = menuItem.detailText
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func addToOrderButtonTapped(_ sender: UIButton) {
+        /// Создаем анимацию для кнопки добавления заказа
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.7,
+            initialSpringVelocity: 0.1,
+            animations: {
+                self.addToOrderButton.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+                self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            },completion: nil)
     }
 }
