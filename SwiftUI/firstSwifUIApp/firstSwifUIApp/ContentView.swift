fff032f7114532340ef0819bb067d36c0ea613d0
pro100ton <pro100ton@gmail.com>
2023-03-31 23:17:23 +0300

    Add SwiftUI first app

---
+++ SwiftUI/firstSwifUIApp/firstSwifUIApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  firstSwifUIApp
+//
+//  Created by Антон Шалимов on 31.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Sashka!")
+            .font(.largeTitle)
+            .italic()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/firstSwifUIApp/firstSwifUIApp/firstSwifUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwifUIAppApp.swift
+//  firstSwifUIApp
+//
+//  Created by Антон Шалимов on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct firstSwifUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
