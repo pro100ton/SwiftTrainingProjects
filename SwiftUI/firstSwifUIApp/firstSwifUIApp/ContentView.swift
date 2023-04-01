f61701bdf56e2f87307e3ccb8038eda223ebb3ec
pro100ton <pro100ton@gmail.com>
2023-04-01 10:47:55 +0300

    Progressing with first SwiftUI app

--- SwiftUI/firstSwifUIApp/firstSwifUIApp/ContentView.swift
+++ SwiftUI/firstSwifUIApp/firstSwifUIApp/ContentView.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Sashka!")
-            .font(.largeTitle)
-            .italic()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
